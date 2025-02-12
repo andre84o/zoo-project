f11b4dfe7bfb2267c807fdbba03eb1b1d1cadfb3
andre84o <andre.torabpour@gmail.com>
2025-02-12 09:27:27 +0100

    merged data to main

---
+++ routes/reptiles.js
@@ -0,0 +1,17 @@
+import express from "express";
+import Animals from "../data/data.js";
+
+const reptilesRouter = express.Router();
+
+reptilesRouter.get("/", (req, res )=> {
+    res.render("pages/animal-page",{
+    
+
+    })
+});
+
+reptilesRouter.get("/")
+
+
+
+export default reptilesRouter;
